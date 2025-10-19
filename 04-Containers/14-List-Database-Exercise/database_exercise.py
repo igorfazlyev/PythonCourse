fd49ae6b8d1a5f0a894e2a7709ffb182195efbbd
igorfazlyev <igorfazlyev@gmail.com>
2025-10-19 20:34:20 +0000

    working on menu

--- 04-Containers/14-List-Database-Exercise/database_exercise.py
+++ 04-Containers/14-List-Database-Exercise/database_exercise.py
@@ -31,3 +31,38 @@ If you select 5, the program quits.
 Until you quit with option 5, the program displays the menu again after every action.
 
 """
+def main():
+    pass
+class Database:
+    def __init__(self,list_of_strings):
+        self.data = list_of_strings
+    def display_data(self):
+        for index, value in enumerate(self.data):
+            print(f"{index}: {value}")
+    def add_item(self):
+        self.data.append(input("enter the new item: "))
+    def delete_item(self):
+        target_index = int(input("enter the index of the item to delete: "))
+        if target_index < 0 or target_index > len(self.data) -1:
+            print("invalid index, nothing's been deleted")
+            return
+        del self.data[target_index]
+    def change_item(self):
+        target_index = int(input("enter the index of the item to be replaced: "))
+        if target_index < 0 or target_index > len(self.data) -1:
+            print("invalid index, nothing's been deleted")
+            return
+        new_value = input("enter the new value for the item: ")
+        self.data[target_index] = new_value
+    def run_database(self):
+        app_is_running = True
+        menu = {
+            "Display database": lambda: self.display_data(),
+            "Add an item": lambda: self.add_item(),
+            "Delete an item": lambda: self.delete_item(),
+            "Change an item": lambda: self.change_item(),
+            "Quit": lambda: app_is_running = False,
+        }
+        while app_is_runnin:
+            
+main()
