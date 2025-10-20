9ba6df23c4583b24561773cfbebc1c2919e77bc2
igorfazlyev <igorfazlyev@gmail.com>
2025-10-20 08:08:32 +0000

    dict exercise

--- 04-Containers/34-Dictionary-Exercise/dictionary_exercise.py
+++ 04-Containers/34-Dictionary-Exercise/dictionary_exercise.py
@@ -16,6 +16,16 @@ Start by putting the names and ages in a dictionary.
 def main():
     people = ["Amelia", "Arthur", "Isla", "Noah", "Ava", "Leo", "Mia", "Oscar"]
     ages = [20, 30, 25, 65, 21, 70, 32, 45]
-
+    db = dict(zip(people, ages))
+    while True:
+        user_input = input("enter the name of the person whose age you want to know or q to quit: ")
+        if user_input.lower() == "q":
+            print("see ya")
+            break
+        age = db.get(user_input,"unknown person")
+        if age == "unknown person":
+            print(age)
+        else:
+            print(f"{user_input}'s {age} years old")
   
 main()
